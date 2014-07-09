0fde7f4d3aafe31e06f2ddc284341d596bc8011b
Anonymous-DevTools-HW <fakefaker@abv.bg>
2014-07-09 13:08:41 +0300

    ConsoleInput implemented

--- BattleField4/BattleFieldGameLib/UserInput/ConsoleInput.cs
+++ BattleField4/BattleFieldGameLib/UserInput/ConsoleInput.cs
@@ -1,22 +1,59 @@
 ﻿namespace BattleFieldGameLib
 {
+    using System;
     public class ConsoleInput : IInputable
     {
         public int GetFieldSize()
         {
-            throw new System.NotImplementedException();
             // FIRST THING WHEN THE GAME STARTS
+            var input = Console.ReadLine();
+            int fieldSize = 0;
+            bool isInteger = int.TryParse(input, out fieldSize);
+
+            while (!isInteger)
+            {
+                Console.WriteLine("You must enter in integer as field size. Please try again: ");
+                GetFieldSize();
+            }
+
+            bool isValidSize = (6 < fieldSize && fieldSize < 40);
+            while (!isValidSize)
+            {
+               Console.WriteLine("The field. Please try again: ");
+               GetFieldSize(); 
+            }
+
+            return fieldSize;
         }
 
         public IPosition GetPositon()
         {
-            throw new System.NotImplementedException();
             // new Position(x,y)
+
+            // Input format - "x y"
+            var input = Console.ReadLine();
+            var components = input.Split(' ');
+            int x, y;
+            bool isValidX = int.TryParse(components[0], out x);
+            bool isValidY = int.TryParse(components[1], out y);
+
+            //Check if input is inside the field;
+            
+            while(!isValidX || !isValidY)
+	        {
+                Console.WriteLine("You have entered an invalid position. Please try again: ");
+                GetPositon();
+            }
+
+            IPosition position = new Position(x, y);
+            return position;
         }
 
         public string GetUsername()
         {
-            throw new System.NotImplementedException();
+            var username = Console.ReadLine();
+
+            return username;
         }
     }
 }
