70d5984af41e78b89d19a2e03e3d841ea9fa6e1d
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2014-07-10 12:39:40 +0300

    created explosion logic

--- BattleField4/BattleFieldGameLib/Engine/Engine.cs
+++ BattleField4/BattleFieldGameLib/Engine/Engine.cs
@@ -9,7 +9,12 @@
         private IDrawer consoleDrawer;
         private User user;
         private GameField gameField;
+        private MineFactory mineFactory = new MineCreator();
+        private int finalScore = 0;
 
+
+        //use dependancy injection(design pattern) for private fields in the engine
+        //show what the engine class uses in order to work
         public Engine()
         {
             consoleReader = new ConsoleInput();
@@ -21,7 +26,9 @@
             // *Intro to the game
             // *Play Music
 
+            consoleDrawer.DrawText("Enter user name: ");
             user = new User(consoleReader.GetUsername()); // User get Nickname USE ConsoleInput
+            consoleDrawer.DrawText("Enter field size: ");
             user.FieldSize = consoleReader.GetFieldSize(); // User get field size USE ConsoleInput
 
             // Menu 
@@ -29,34 +36,101 @@
             gameField = new GameField(user.FieldSize); // Generate field matrix USE GameField
 
             // Generate mines USE MineCreator
-            PopulateField(); // Populate field matrix USE GameField.Indexer
+            int minesOnFieldCount = PopulateField(); // Populate field matrix USE GameField.Indexer
             // Draw ingame menu (star/stop music)
 
-            while (true)
+            while (minesOnFieldCount > 0)
             {
                 consoleDrawer.Clear();
-                ShowLastHit();
+                ShowLastHit();//TODO: FIX -> cant easely see what this method needs to do it's work
 
                 consoleDrawer.DrawObject(gameField); // Draw field USE IDrawer.DrawObject
 
-                AskForPosition();
-                user.LastInput = consoleReader.GetPositon(); // Ask user for attack position/coordinates USE ConsoleInput
+                AskForPosition();//TODO: FIX -> cant easely see what this method needs to do it's work
 
-                while (!IsValidPosition())
+                user.LastInput = consoleReader.GetPositon(); // Ask user for attack position/coordinates USE ConsoleInput
+                finalScore++; // after user makes a choise finalScore++ (depends on user tries)
+
+                while (!IsValidPosition()) //TODO: FIX -> cant easely see what this method needs to do it's work
                 {
-                    AskForPosition();
+                    AskForPosition();//TODO: FIX -> cant easely see what this method needs to do it's work
                     user.LastInput = consoleReader.GetPositon();
                 }
 
-                // Hit {
-                // Count destroyed filed/mines USE MineBody
-                // Count user score
-                // Calculate hit and change the field matrix USE MineBody
-                // }
+                bool isMineHit = IsMineHit();//TODO: FIX -> cant easely see what this method needs to do it's work
+
+                if (isMineHit)
+                {
+                    //TODO: implement strategy: explosion manager class takes different explosion strategies
+                    // needs to be something like: currentMine.Explode(); inside explosion manager
+                    //explosionManager(currentMine, gameField);
+                    //explosionManager.handleExplosion();
+                    int minesTakenOut = Explode();//TODO: FIX -> cant easely see what this method needs to do it's work
+                    minesOnFieldCount -= minesTakenOut;
+                }
             }
+
+            //TODO: change with highscore logic
+            consoleDrawer.DrawText(string.Format("You made it with: {0} tries", finalScore));//this is only a test
 
             // Save Highscore USE HighScore
             // Show highscore USE HighScore
+        }
+
+        private bool IsMineHit()
+        {
+            char fieldHit = gameField[user.LastInput.PosX, user.LastInput.PosY];
+
+            if (fieldHit == 0 || fieldHit == '*')
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private int Explode()
+        {
+            int mineHit = int.Parse(gameField[user.LastInput.PosX, user.LastInput.PosY].ToString());
+            var fieldLength = gameField.FieldBody.GetLength(0) - 1;
+            var offsetX = user.LastInput.PosX - 2;
+            var offsetY = user.LastInput.PosY - 2;
+
+            //make me a mine and give me it's blast area
+            var currentMineBody = mineFactory.CreateMine((MinePower)mineHit).GetBlastArea();
+
+            int minesTakenOut = 0;
+            //walks through every field of the mines' blast area
+            for (int row = 0; row < currentMineBody.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentMineBody.GetLength(1); col++)
+                {
+                    var rowField = row + offsetX;
+                    var colField = col + offsetY;
+
+                    //don't do anything if you're not in the game field
+                    if (rowField < 0 ||  fieldLength < rowField ||
+                        colField < 0 || fieldLength < colField)
+                    {
+                        continue;
+                    }
+
+                    //if the blast area covers this field
+                    if (currentMineBody[row, col] == 1)
+                    {
+                        //if there is a mine ... take it out
+                        if ((gameField[rowField, colField] != 0) &&
+                            (gameField[rowField, colField] != '*'))
+                        {
+                            minesTakenOut++;
+                        }
+
+                        //mark game field as blasted
+                        gameField[rowField, colField] = '*';
+                    }
+                }
+            }
+
+            return minesTakenOut;
         }
 
         private void AskForPosition()
@@ -84,7 +158,7 @@
             return false;
         }
 
-        public void PopulateField()
+        public int PopulateField()
         {
             int fieldSize = this.user.FieldSize;
 
@@ -100,8 +174,10 @@
                     y = randomNum.Next(0, fieldSize);
                 }
 
-                this.gameField.FieldBody[x, y] = randomNum.Next(1, 6).ToString()[0]; // TO DO: FIX HACK
+                this.gameField.FieldBody[x, y] = randomNum.Next(1, 6).ToString()[0]; // TODO: FIX HACK
             }
+
+            return minesToCreate;
         }
     }
 }
--- BattleField4/BattleFieldGameLib/Interfaces/IDrawer.cs
+++ BattleField4/BattleFieldGameLib/Interfaces/IDrawer.cs
@@ -4,7 +4,6 @@
     {
         void DrawObject(IDrawable drawableObject);
         void DrawText(string text);
-
         void Clear();
     }
 }
