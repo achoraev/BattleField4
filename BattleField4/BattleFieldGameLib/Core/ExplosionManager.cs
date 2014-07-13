d9bc0ab2ab8c22c89ae927f4fe6ab3ccce714b51
Martin <sirjordan1988@gmail.com>
2014-07-13 14:30:28 +0300

    Refactor Explosion manager.

--- BattleField4/BattleFieldGameLib/Core/ExplosionManager.cs
+++ BattleField4/BattleFieldGameLib/Core/ExplosionManager.cs
@@ -6,38 +6,90 @@
 
     public class ExplosionManager : IExplosionManager
     {
-        private const char defaultFieldBlastRepresentation = 'X';
+        private const char DEFAULT_FIELD_BLAST_REPRESENTATION = 'X';
 
         private IGameField gameField;
         private IExplodable currentMine;
         private IPosition currentPosition;
 
+        private IExplodable CurrentMine
+        {
+            get { return this.currentMine; }
+            set
+            {
+                if (value != null)
+                {
+                    this.currentMine = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Invalid parameter, null passed as a 'CurrentMine'");
+                }
+            }
+        }
+
+        private IPosition CurrentPosition
+        {
+            get { return this.currentPosition; }
+            set
+            {
+                if (value != null)
+                {
+                    this.currentPosition = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Invalid parameter, null passed as a 'CurrentPosition'");
+                }
+            }
+        }
+
+        private IGameField GameField
+        {
+            get { return this.gameField; }
+            set
+            {
+                if (value != null)
+                {
+                    this.gameField = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Invalid parameter, null passed as a 'GameField'");
+                }
+            }
+        }
+
+        public char FieldBlastRepresentation { get; set; }
+
         public ExplosionManager(IGameField gameField)
         {
-            this.FieldBlastRepresentation = defaultFieldBlastRepresentation;
-            this.gameField = gameField;
-            this.currentMine = null;
-            this.currentPosition = null;
+            this.FieldBlastRepresentation = DEFAULT_FIELD_BLAST_REPRESENTATION;
+            this.GameField = gameField;
+            //this.CurrentMine = null;    // Unnesesery, the null is deafult value
+            //this.CurrentPosition = null;    // Unnesesery, the null is deafult value
         }
-        
-        public char FieldBlastRepresentation { get; set; }
 
         public void SetHitPosition(IPosition position)
         {
-            this.currentPosition = position;
+            this.CurrentPosition = position;
         }
 
         public void SetMine(IExplodable mine)
         {
-            this.currentMine = mine;
+            this.CurrentMine = mine;
         }
 
-        public int HandleExplosion()
+        /// <summary>
+        /// Walks through every field of the mines' blast area, if there is a mine ... take it out, mark game field as blasted
+        /// </summary>
+        /// <returns>The number of mines taken out by the current mine blast area</returns>
+        public int HandleExplosion()    // TODO: Rename this method
         {
-            int fieldLength = this.gameField.FieldBody.GetLength(0) - 1;
-            int offsetX = this.currentPosition.PosX - 2;
-            int offsetY = this.currentPosition.PosY - 2;
-            int[,] mineBody = this.currentMine.GetBlastArea();
+            int fieldLength = this.GameField.FieldBody.GetLength(0) - 1;
+            int offsetX = this.CurrentPosition.PosX - 2;
+            int offsetY = this.CurrentPosition.PosY - 2;
+            int[,] mineBody = this.CurrentMine.GetBlastArea();
 
             int minesTakenOut = 0;
 
@@ -46,12 +98,11 @@
             {
                 for (int col = 0; col < mineBody.GetLength(1); col++)
                 {
-                    var rowField = row + offsetX;
-                    var colField = col + offsetY;
+                    int rowField = row + offsetX;
+                    int colField = col + offsetY;
 
                     // don't do anything if you're not in the game field
-                    if (rowField < 0 || fieldLength < rowField ||
-                        colField < 0 || fieldLength < colField)
+                    if (rowField < 0 || fieldLength < rowField || colField < 0 || fieldLength < colField)
                     {
                         continue;
                     }
@@ -60,19 +111,33 @@
                     if (mineBody[row, col] == 1)
                     {
                         // if there is a mine ... take it out
-                        if ((this.gameField[rowField, colField] != 0) &&
-                            (this.gameField[rowField, colField] != FieldBlastRepresentation))
+                        if (IsThereMineIn(rowField, colField))
                         {
                             minesTakenOut++;
                         }
 
                         // mark game field as blasted
-                        this.gameField[rowField, colField] = FieldBlastRepresentation;
+                        MarkFieldAsBlasted(rowField, colField);
                     }
                 }
             }
 
             return minesTakenOut;
+        }
+
+        private void MarkFieldAsBlasted(int row, int col)
+        {
+            this.GameField[row, col] = FieldBlastRepresentation;
+        }
+
+        private bool IsThereMineIn(int row, int col)
+        {
+            if ((this.gameField[row, col] != 0) && (this.gameField[row, col] != FieldBlastRepresentation))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
