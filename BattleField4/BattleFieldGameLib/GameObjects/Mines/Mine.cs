60d4b71200db695c923b0ec9e37da792940d3715
d-kostov-dev <d.kostov.dev@gmail.com>
2014-07-14 22:11:41 +0300

    Mine base class - Documentation

--- BattleField4/BattleFieldGameLib/GameObjects/Mines/Mine.cs
+++ BattleField4/BattleFieldGameLib/GameObjects/Mines/Mine.cs
@@ -4,14 +4,30 @@
     using BattleFieldGameLib.Enums;
     using BattleFieldGameLib.Interfaces;
     
+    /// <summary>
+    /// Mines base class. All mines inherit this class
+    /// </summary>
     public abstract class Mine : IMine, IExplodable
     {
+        /// <summary>
+        /// Power of the mine.
+        /// </summary>
         public MinePower Power { get; set; }
 
+        /// <summary>
+        /// The "visualisation" of the current bomb explosion
+        /// </summary>
         protected int[,] MineBody { get; set; }
 
+        /// <summary>
+        /// Returns mine body. Used method for easy extension or changes in future.
+        /// </summary>
+        /// <returns>Current mine body integer matrix</returns>
         public abstract int[,] GetBlastArea();
 
+        /// <summary>
+        /// Method needed for the mine factory. Creates a mine.
+        /// </summary>
         public abstract void CreateMine();
     }
 }
