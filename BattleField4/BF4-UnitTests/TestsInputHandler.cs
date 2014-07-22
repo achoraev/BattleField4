e0ab3fd6aa99c0dc6aa53c8709d68464e7acc8c0
ADimanova <addi_91@abv.bg>
2014-07-22 13:38:04 +0300

    Moq Tests

--- BattleField4/BF4-UnitTests/TestsInputHandler.cs
+++ BattleField4/BF4-UnitTests/TestsInputHandler.cs
@@ -1,128 +1,46 @@
 ﻿namespace BF4_UnitTests
 {
     using System;
+    using Moq;
     using BattleFieldGameLib.Common;
     using BattleFieldGameLib.Interfaces;
     using BattleFieldGameLib.Renderer;
     using BattleFieldGameLib.UserInput;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    
-    /// <summary>
-    /// Fake class created for the purpose of testing the InputHandler class.
-    /// </summary>
-    public class FakeReader : IInputable
-    {
-        /// <summary>
-        /// Holds a test field size value.
-        /// </summary>
-        public int Fieldsize { get; set; }
-
-        /// <summary>
-        /// Holds a test position value.
-        /// </summary>
-        public IPosition Position { get; set; }
-
-        /// <summary>
-        /// Holds a test choise of menu value.
-        /// </summary>
-        public int MenuChoise { get; set; }
-
-        /// <summary>
-        /// Holds a test username value.
-        /// </summary>
-        public string UserName { get; set; }
-
-        /// <summary>
-        /// Gets a test value for FieldSize depending on some predetermined conditions.
-        /// </summary>
-        /// <returns>A test value for the size of the field.</returns>
-        public int GetFieldSize()
-        {
-            var result = this.Fieldsize;
-            if (this.Fieldsize <= InputHandler.MinFieldSize)
-            {
-                this.Fieldsize++;
-            }
-            else if (this.Fieldsize >= InputHandler.MaxFieldSize)
-            {
-                this.Fieldsize--;
-            }
-
-            return result;
-        }
-
-        bool isNullTest = false;
-
-        /// <summary>
-        /// Gets a test value for Position depending on some predetermined conditions.
-        /// </summary>
-        /// <returns>A test value for a position on the field.</returns>
-        public IPosition GetPositon()
-        {
-            var result = this.Position;
-            if (!isNullTest && this.Position == null)
-            {
-                isNullTest = true;
-            }
-
-            if (isNullTest)
-            {
-                result = new Position(1, 1);
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// Gets a test value for Username depending on some predetermined conditions.
-        /// </summary>
-        /// <returns>A test value for username.</returns>
-        public string GetUsername()
-        {
-            return this.UserName;
-        }
-
-        /// <summary>
-        /// Gets a test value for MenuChoise depending on some predetermined conditions.
-        /// </summary>
-        /// <returns>A test value for a choise of menu.</returns>
-        public int GetMenuChoice()
-        {
-            return this.MenuChoise;
-        }
-    }
 
     /// <summary>
     /// Test InputHandler class.
     /// </summary>
     [TestClass]
     public class TestsInputHandler
-    {    
+    {
         //Fieldsize method tests.
         /// <summary>
         /// Testing GetFieldSize method with fieldsize equal to eight.
         /// </summary>
+
         [TestMethod]
         public void SuccessfulPassAtATrivialCaseOfGetFieldSize()
         {
             IDrawer drawer = new ConsoleRenderer();
-            FakeReader reader = new FakeReader();
-            reader.Fieldsize = 8;
-            var inputHandler = new InputHandler(drawer, reader);
+            var fakeReader = new Mock<IInputable>();
+            fakeReader.Setup(x => x.GetFieldSize()).Returns(8);
+            var inputHandler = new InputHandler(drawer, fakeReader.Object);
 
             var result = inputHandler.GetFieldSize();
             Assert.AreEqual(result, 8);
         }
 
-        /// <summary>
-        /// Testing GetFieldSize with the smallest allowed value.
-        /// </summary>
         [TestMethod]
         public void SuccessfulPassAtFieldSizeAboveMinSizeOfGetFieldSize()
         {
             IDrawer drawer = new ConsoleRenderer();
-            FakeReader reader = new FakeReader();
-            reader.Fieldsize = 0;
-            var inputHandler = new InputHandler(drawer, reader);
+            var fakeReader = new Mock<IInputable>();
+            var value = 0;
+            fakeReader.Setup(x => x.GetFieldSize())
+                .Returns(() => value)
+                .Callback(() => value++);
+            var inputHandler = new InputHandler(drawer, fakeReader.Object);
 
             var result = inputHandler.GetFieldSize();
             Assert.AreEqual(result, InputHandler.MinFieldSize + 1);
@@ -135,9 +53,12 @@
         public void SuccessfulPassAtFieldSizeBelowMaxSizeOfGetFieldSize()
         {
             IDrawer drawer = new ConsoleRenderer();
-            FakeReader reader = new FakeReader();
-            reader.Fieldsize = InputHandler.MaxFieldSize + 10;
-            var inputHandler = new InputHandler(drawer, reader);
+            var fakeReader = new Mock<IInputable>();
+            var value = InputHandler.MaxFieldSize + 10;
+            fakeReader.Setup(x => x.GetFieldSize())
+                .Returns(() => value)
+                .Callback(() => value--);
+            var inputHandler = new InputHandler(drawer, fakeReader.Object);
 
             var result = inputHandler.GetFieldSize();
             Assert.AreEqual(result, InputHandler.MaxFieldSize - 1);
@@ -151,9 +72,9 @@
         public void TestingCorrectPositionBehaviour()
         {
             IDrawer drawer = new ConsoleRenderer();
-            FakeReader reader = new FakeReader();
-            reader.Position = new Position(5, 5);
-            var inputHandler = new InputHandler(drawer, reader);
+            var fakeReader = new Mock<IInputable>();
+            fakeReader.Setup(x => x.GetPositon()).Returns(new Position(5, 5));
+            var inputHandler = new InputHandler(drawer, fakeReader.Object);
 
             var result = inputHandler.GetPositon();
             Assert.IsTrue(result.PosX == 5 && result.PosY == 5);
@@ -166,14 +87,17 @@
         public void TestingNullPositionBehaviour()
         {
             IDrawer drawer = new ConsoleRenderer();
-            FakeReader reader = new FakeReader();
-            reader.Position = null;
-            var inputHandler = new InputHandler(drawer, reader);
+            var fakeReader = new Mock<IInputable>();
+            Position value = null;
+            fakeReader.Setup(x => x.GetPositon())
+                .Returns(() => value)
+                .Callback(() => value = new Position(1, 1));
+            var inputHandler = new InputHandler(drawer, fakeReader.Object);
 
             var result = inputHandler.GetPositon();
             Assert.IsTrue(result.PosX == 1 && result.PosY == 1);
         }
 
-        //TODO MenuChoice
+        //TODO: MenuCoice Tests
     }
 }
